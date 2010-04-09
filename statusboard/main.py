954470cae323f37befd820cf1ef71a4a1c0d5196
Noah Kantrowitz <noah@coderanger.net>
2010-04-09 00:42:34 -0700

    Layout storage is working.

--- statusboard/main.py
+++ statusboard/main.py
@@ -1,5 +1,5 @@
 import random
-
+import simplejson
 from juno import *
 import werkzeug
 init({ 'db_type': 'sqlite', 
@@ -11,6 +11,8 @@ init({ 'db_type': 'sqlite',
        'use_sessions': True,
        'session_secret': 'ASDFQW$FAQWEFASD',
      })
+
+from utils import hex_random
      
 Builder = model('Builder', 
     name='string',
@@ -24,15 +26,30 @@ Builder = model('Builder',
     last_build='string',
     gimme_branch='string',
     svn_branch='string'
-    )
+)
 
 Settings = model('Settings',
     user='string',
     grid='string' # JSON encoded data
-    )
+)
+
+default_grid = [
+    [
+        {'type': 'label', 'id': '1'}, 
+        {'type': 'builder', 'id': '2'},
+        {'type': 'builder', 'id': '3'},
+    ],
+    [
+        {'type': 'label', 'id': '4'},
+        {'type': 'builder', 'id': '5'},
+        {'type': 'builder', 'id': '6'},
+    ],
+]
+default_grid_json = simplejson.dumps(default_grid)
 
 def render_builder(opts):
     data = {
+        'id': opts['id'],
         'label': 'CONT1',
         'progress': random.randint(1, 100),
     }
@@ -40,31 +57,59 @@ def render_builder(opts):
 
 def render_label(opts):
     data = {
+        'id': opts['id'],
         'label': 'FC',
     }
     return render_template(get_template('widget_label.html'), **data)
 
-def hex_random(n):
-    byte = lambda: '%02x'%random.randint(0, 255)
-    return ''.join(byte() for _ in xrange(n))
-
-@route('/')
-def index(web):
+def load_settings(web):
     if 'name' not in web.session:
         web.session['name'] = hex_random(6)
         web.session.save()
-    grid = [
-        [{'type': 'label'}, {'type': 'builder'}, {'type': 'builder'},],
-        [{'type': 'label'}, {'type': 'builder'}, {'type': 'builder'},  ],
-    ]
-    for row in grid:
+    user_name = web.session['name']
+    user_settings = Settings.find().filter_by(user=user_name).first()
+    if not user_settings:
+        user_settings = Settings(user=user_name, grid=default_grid_json)
+        user_settings.save()
+    web.grid = simplejson.loads(user_settings.grid)
+    
+    # For debugging
+    if web.input('reset'):
+        user_settings.grid = default_grid_json
+        user_settings.save()
+        web.grid = default_grid
+    return user_settings
+
+@route('/')
+def index(web):
+    user_settings = load_settings(web)
+    for row in web.grid:
         for widget_opts in row:
             if widget_opts['type'] == 'label':
                 fn = render_label
             elif widget_opts['type'] == 'builder':
                 fn = render_builder
             widget_opts['output'] = fn(widget_opts)
-    template('index.html', {'grid': grid, 'name': web.session['name']})
+    template('index.html', {'grid': web.grid})
+
+@route('/ajax/layout')
+def ajax_layout(web):
+    user_settings = load_settings(web)
+    widgets = {}
+    for row in web.grid:
+        for widget in row:
+            widgets[widget['id']] = widget
+    
+    new_grid = []
+    row_count = web.input('rows')
+    for i in xrange(int(row_count)):
+        new_row = []
+        for widget_id in web.input('row%s'%i):
+            if widget_id in widgets:
+                new_row.append(widgets[widget_id])
+        new_grid.append(new_row)
+    user_settings.grid = simplejson.dumps(new_grid)
+    user_settings.save()
 
 @route('/mock')
 def mock(web):
---
+++ statusboard/utils.py
@@ -0,0 +1,22 @@
+import random
+
+def hex_random(n):
+    byte = lambda: '%02x'%random.randint(0, 255)
+    return ''.join(byte() for _ in xrange(n))
+
+def add_new_widget(grid, add_row, add_after):
+    new_id = hex_random(4)
+    for row in grid:
+        for widget in row:
+            if widget['id']  == new_id:
+                # Duplicate ID, start over
+                return add_new_widget(grid, add_row, add_after)
+    new_widget = {'id': new_id}
+    while add_row >= len(grid):
+        grid.append([])
+    for i, widget in enumerate(grid[add_row]):
+        if widget['id'] == add_after:
+            grid[add_row].insert(i+1, new_widget)
+            return new_widget
+    grid[add_row].append(new_widget)
+    return new_widget
