d7833c385b8ff419f0045986748f5a279bfd0fa3
Noah Kantrowitz <noah@coderanger.net>
2010-04-21 01:23:38 -0700

    Move the plugin's static files from /plugin to /plugin/static. /plugin/:name can now be used for plugin-specific handlers more safely.

--- statusboard/main.py
+++ statusboard/main.py
@@ -172,7 +172,7 @@ def debug_grid(web):
 def favicon(web):
     static_serve(web, 'favicon.ico')
 
-@route('/plugin/:plugin/:file')
+@route('/plugin/static/:plugin/:file')
 def plugin_static(web, plugin, file):
     plugin = plugin_registry.get(plugin)
     if plugin is None:
