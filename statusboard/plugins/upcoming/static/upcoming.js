445a8dc2b5fc32dbd3237b4e5ab483c3642daa24
Noah Kantrowitz <noah@coderanger.net>
2010-04-25 02:01:51 -0700

    Loop the title when it goes off screen.

--- statusboard/plugins/upcoming/static/upcoming.js
+++ statusboard/plugins/upcoming/static/upcoming.js
@@ -1,9 +1,11 @@
 $(function() {
-  $('.upcoming .title').wrapInner('<div class="title-inner"></div>').live('mouseenter', function() {
+  $('.upcoming .title').wrapInner('<div class="title-inner"><span></span></div>').live('mouseenter', function() {
     var self = $(this).find('.title-inner');
     function scroll() {
       var new_margin = self.data('title-scroll-margin') + 1;
-      console.log(new_margin + ',' + self.width());
+      if(new_margin >= self.find('span').width()) {
+        new_margin = 0;
+      }
       self.css('margin-left', '-'+new_margin+'px');
       self.data('title-scroll-margin', new_margin);
       self.data('title-scroll', setTimeout(scroll, 30));
