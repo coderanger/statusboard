c546b59a2dc20b5e808d1688b406af9dbbea5f03
Noah Kantrowitz <noah@coderanger.net>
2010-04-25 19:46:27 -0700

    Update UI for the Upcoming plugin. Now the scrolling is setup automatically for titles that are too long. Improved CSS.

--- statusboard/plugins/upcoming/static/upcoming.js
+++ statusboard/plugins/upcoming/static/upcoming.js
@@ -1,5 +1,7 @@
 $(function() {
-  $('.upcoming .title.long').wrapInner('<div class="title-inner"><span></span></div>').live('mouseenter', function() {
+  $('.upcoming').live('statusboard-reload', function() {
+    $(this).find('.event .title').wrapInner('<div class="title-inner"><span></span></div>');
+  }).trigger('statusboard-reload').find('.event .title').live('mouseenter', function() {
     var self = $(this).find('.title-inner');
     function scroll() {
       var new_margin = self.data('title-scroll-margin') + 1;
@@ -10,7 +12,9 @@ $(function() {
       self.data('title-scroll-margin', new_margin);
       self.data('title-scroll', setTimeout(scroll, 30));
     }
-    self.data('title-scroll', setTimeout(scroll, 30)).data('title-scroll-margin', 0);
+    if(self.height() > 22) {
+      self.data('title-scroll', setTimeout(scroll, 30)).data('title-scroll-margin', 0);
+    }
   }).live('mouseleave', function() {
     var inner = $(this).find('.title-inner').css('margin-left', '0');
     clearTimeout(inner.data('title-scroll'));
