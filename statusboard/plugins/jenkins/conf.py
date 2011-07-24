a9dee9086d6dc978a7fa91d86a149f79866f7683
Noah Kantrowitz <noah@coderanger.net>
2011-07-23 20:12:04 -0700

    Config option for jenkins server timezone.

--- statusboard/plugins/jenkins/conf.py
+++ statusboard/plugins/jenkins/conf.py
@@ -3,3 +3,5 @@ from django.conf import settings
 AUTOSERVERS = getattr(settings, 'STATUSBOARD_JENKINS_AUTOSERVERS', True)
 
 SERVERS = getattr(settings, 'STATUSBOARD_JENKINS_SERVERS', [])
+
+TIME_ZONE = getattr(settings, 'STATUSBOARD_JENKINS_TIME_ZONE', getattr(settings, 'TIME_ZONE', 'UTC'))
